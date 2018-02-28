1f5e44907fc78a6531eb3a88fb446a65f7f7e57d
Abarbasam <samabarbanel1@gmail.com>
2018-02-28 17:24:30 -0400

    Cleaned up files

--- Good_Morning.js
+++ Good_Morning.js
@@ -31,7 +31,9 @@ var lengthOfList = messages.length;
 
 	// To find your friends ID, go to their page, click on their profile picture, and right click on the description box above
 	// the comment box. Select "View page source," and then hit CTRL-f. Search for "sharer_id", and the ID will be right after it
-	var friendsID = "1000000000" // This needs to be changed
+	
+    var friendsID = "1000000000" // FILL YOUR FRIENDS FACEBOOK ID HERE!
+
 	var msg = {body: messages[(day + lengthOfList) % lengthOfList]}
 
 	 api.sendMessage(msg, friendsID)
